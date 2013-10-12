59a394cd6aa90f74a83da1b48a513c72c98c82e3
VictorGuzman <viaguz@gmail.com>
2013-10-12 16:29:52 -0600

    Adds validations for stores and articles.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ActiveRecord::Base
 	belongs_to :store
+	validates_presence_of :name
+	validates_length_of :name, :maximum => 30
+	validates_presence_of :description
+	validates_presence_of :price
+	validates_numericality_of :price
+	validates_numericality_of :total_in_shelf
+	validates_numericality_of :total_in_vault
+	validates_presence_of :store_id
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,6 @@
 class Store < ActiveRecord::Base
 	has_many :articles
+	validates_presence_of :name
+	validates_length_of :name, :maximum => 30
+	validates_presence_of :address
 end

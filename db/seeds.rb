16567fef9e2d5268552dcd79a180b4d645d35260
VictorGuzman <viaguz@gmail.com>
2013-10-12 18:51:47 -0600

    Adds a proper seed.rb file to pupulate the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,56 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Article.delete_all
-Store.delete_all
+superShoesSJ = Store.create([{name: 'Supershoes San Jose',    address: 'San Antonio Desamparados'}])
+superShoesAL = Store.create([{name: 'Supershoes Alajuela',    address: 'Near the park'}])
+superShoesCA = Store.create([{name: 'Supershoes Cartago',     address: 'Near The Mutual'}])
+superShoesHE = Store.create([{name: 'Supershoes Heredia',     address: 'In front of Soccer Stadium'}])
+superShoesGA = Store.create([{name: 'Supershoes Guanacaste',  address: '3km before Liberia'}])
+superShoesPT = Store.create([{name: 'Supershoes Puntarenas',  address: 'Next to Paseo de los Turistas'}])
+superShoesLM = Store.create([{name: 'Supershoes Limon',  	  address: 'Near downtown'}])
+
+for i in 1..6
+	o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+	rand_string = (0...5).map{ o[rand(o.length)] }.join
+	Article.create([{name: "Art_#{rand_string}", description: "Desc for Art#{rand_string}", price: ((i*100)-16), total_in_shelf: ((i*12)-4), total_in_vault: ((i*12)-5), store_id: superShoesSJ.first.id}])
+end
+
+for i in 1..6
+	o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+	rand_string = (0...5).map{ o[rand(o.length)] }.join
+	Article.create([{name: "Art_#{rand_string}", description: "Desc for Art#{rand_string}", price: ((i*100)-16), total_in_shelf: ((i*12)-4), total_in_vault: ((i*12)-5), store_id: superShoesAL.first.id}])
+end
+
+for i in 1..6
+	o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+	rand_string = (0...5).map{ o[rand(o.length)] }.join
+	Article.create([{name: "Art_#{rand_string}", description: "Desc for Art#{rand_string}", price: ((i*100)-16), total_in_shelf: ((i*12)-4), total_in_vault: ((i*12)-5), store_id: superShoesCA.first.id}])
+end
+
+for i in 1..6
+	o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+	rand_string = (0...5).map{ o[rand(o.length)] }.join
+	Article.create([{name: "Art_#{rand_string}", description: "Desc for Art#{rand_string}", price: ((i*100)-16), total_in_shelf: ((i*12)-4), total_in_vault: ((i*12)-5), store_id: superShoesHE.first.id}])
+end
+
+for i in 1..6
+	o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+	rand_string = (0...5).map{ o[rand(o.length)] }.join
+	Article.create([{name: "Art_#{rand_string}", description: "Desc for Art#{rand_string}", price: ((i*100)-16), total_in_shelf: ((i*12)-4), total_in_vault: ((i*12)-5), store_id: superShoesGA.first.id}])
+end
+
+for i in 1..6
+	o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+	rand_string = (0...5).map{ o[rand(o.length)] }.join
+	Article.create([{name: "Art_#{rand_string}", description: "Desc for Art#{rand_string}", price: ((i*100)-16), total_in_shelf: ((i*12)-4), total_in_vault: ((i*12)-5), store_id: superShoesPT.first.id}])
+end
+
+for i in 1..6
+	o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+	rand_string = (0...5).map{ o[rand(o.length)] }.join
+	Article.create([{name: "Art_#{rand_string}", description: "Desc for Art#{rand_string}", price: ((i*100)-16), total_in_shelf: ((i*12)-4), total_in_vault: ((i*12)-5), store_id: superShoesLM.first.id}])
+end
+
+# If want to clean up database, uncomment next two lines:
+# Store.delete_all
+# Article.delete_all
